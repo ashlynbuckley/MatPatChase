853b51472d253443c86f0e81e7872d3477d4efc7
Ashlyn Buckley <147057014+ashlynbuckley@users.noreply.github.com>
2024-04-02 19:12:53 +0100

    weight and height

--- MatPatChase/src/MatPatChase/Player.java
+++ MatPatChase/src/MatPatChase/Player.java
@@ -6,6 +6,8 @@ public class Player extends Sprites{
 	
 	public Player(Image i, int x, int y) {
 		super(i, y, y);
+		width = gameImage.getWidth(null);
+		height = gameImage.getHeight(null);
 	}
 
 }
