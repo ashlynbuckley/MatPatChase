db42a48c25858acd77dc7f0464b506bdde5cfc8a
Ashlyn Buckley <147057014+ashlynbuckley@users.noreply.github.com>
2024-04-03 17:32:48 +0100

    Update MatPat.java

--- MatPatChase/src/MatPatChase/MatPat.java
+++ MatPatChase/src/MatPatChase/MatPat.java
@@ -7,16 +7,28 @@ public class MatPat extends Sprites{
 	public MatPat(Image i, int x, int y) {
 		super(i, y, y);
 		width = gameImage.getWidth(null);
-		height = gameImage.getHeight(null);
+        height = gameImage.getHeight(null);
 	}
 	
-	@Override
-	public void move(){
-		//A* pathfinding
-        x += xSpeed;
-        y += ySpeed;
+	//pathfinding alg
+	public void move(int targx, int targy) {
+		int speed = 2;
+		
+		//use the target's x and y  to determine direction
+        if (targx<x) {
+            x = x - speed;
+        }
+        else if (targx>x) {
+            x = x + speed;
+        }
+        if (targy<y) {
+            y = y - speed;
+        }
+        else if (targy>y) {
+            y = y + speed;
+        }
     }
-
+	
 	public boolean checkCollision(Sprites other) {
 		//matpat's data
 		int x1 = (int)this.x;
@@ -30,6 +42,7 @@ public class MatPat extends Sprites{
 		int w2 = other.width;
 		int h2 = other.height;
 		
+		//overlapping coordinates
 	    if (
 	        ( (x1<x2 && x1+w1>x2) || (x2<x1 && x2+w2>x1)
 	        )
@@ -37,7 +50,7 @@ public class MatPat extends Sprites{
 	        ( (y1<y2 && y1+h1>y2) || (y2<y1 && y2+h2>y1) )
 	        ) {
 	        return true;}
-        
+        //not overlapping return false, boolean used in main
         return false;
 	}
 }
